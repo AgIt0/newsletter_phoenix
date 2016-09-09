25d20e03ebc830dc1a24a55e0a879cacaaac0620
Lyuben Petrov <lyuben.y.petrov@gmail.com>
2016-09-09 20:30:05 +0300

    Update seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,4 +1,5 @@
-alias NewsletterPhoenix.{Repo, User, Category}
+alias NewsletterPhoenix.{Repo, User, Category, Newsletter, Link}
+import Ecto.Query
 
 users = [
   %{
@@ -11,11 +12,30 @@ users = [
 
 for user <- users do
   Repo.get_by(User, email: user.email) ||
-  Repo.insert(User.changeset(%User{}, user))
+    Repo.insert(User.changeset(%User{}, user))
 end
 
 
 for category <- ~w(Ruby C# JS Others) do
   Repo.get_by(Category, name: category) ||
-  Repo.insert(%Category{name: category})
+    Repo.insert(%Category{name: category})
+end
+
+test_newsletter = Repo.get_by(Newsletter, name: "test") ||
+                    Repo.insert(%Newsletter{name: "test"})
+first_user = User |> first |> Repo.one
+first_category = Category |> first |> Repo.one
+
+for i <- 1..5 do
+    title = "Google#{i}"
+    Repo.get_by(Link, title: title) ||
+      Repo.insert!(
+        %Link{
+          user_id: first_user.id,
+          url: "http://google.com",
+          title: title,
+          newsletter_id: test_newsletter.id,
+          comment: "Hello Google",
+          category_id: first_category.id
+        })
 end
