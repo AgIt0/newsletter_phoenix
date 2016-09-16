750c2ef421d00e2495e7fde60c736d0ac767d16f
Lyuben Petrov <lyuben.y.petrov@gmail.com>
2016-09-17 01:05:37 +0300

    Helper to create newsletters

--- web/models/newsletter.ex
+++ web/models/newsletter.ex
@@ -1,9 +1,10 @@
 defmodule NewsletterPhoenix.Newsletter do
   use NewsletterPhoenix.Web, :model
 
+  alias NewsletterPhoenix.{Newsletter, Link}
   schema "newsletters" do
     field :name, :string
-    has_many :links, NewsletterPhoenix.Link
+    has_many :links, Link
 
     timestamps()
   end
@@ -15,5 +16,10 @@ defmodule NewsletterPhoenix.Newsletter do
     struct
     |> cast(params, [:name])
     |> validate_required([:name])
+  end
+
+  def insert_newsletter do
+    links = Repo.all(from u in Link, where: is_nil(u.newsletter_id))
+    Repo.insert!(%Newsletter{name: Enum.join(Tuple.to_list(:calendar.iso_week_number), "-"), links: links})
   end
 end
